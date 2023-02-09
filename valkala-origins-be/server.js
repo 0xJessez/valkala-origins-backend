d3345ccfdf9323eb2b4dbbd2d8cd9daf377e367a
0xJessez <jesse.zhang919@gmail.com>
2023-02-09 14:07:40 +1100

    server modified for deployment

--- valkala-origins-be/server.js
+++ valkala-origins-be/server.js
@@ -26,3 +26,12 @@ app.use(sessions)
 app.use('/api/users', usersController)
 app.use('/api/sessions', sessionsController)
 app.use('/api/orcs', orcsController)
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
